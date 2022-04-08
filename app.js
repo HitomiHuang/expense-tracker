292774746ac35030b2c1f3f9d4e83d473850c86e
HitomiHuang <black011602@gmail.com>
2022-04-08 22:37:21 +0800

    feat: add handlebars and routes

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const routes = require('./routes')
 require('./config/mongoose')
-
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = 3000
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`)
