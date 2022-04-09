9da6fb8468e13194538044a9f99d254a5f901c79
HitomiHuang <black011602@gmail.com>
2022-04-09 19:29:21 +0800

    feat: add user registration feature

--- models/user.js
+++ models/user.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-  userId:{
-    type: Number,
-    required:true
-  },
+  // userId:{
+  //   type: Number,
+  //   required:true
+  // },
   name:{
     type: String,
     required: true
