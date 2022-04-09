ad652dfd73e20e723609d518f256de358d0d6e5f
HitomiHuang <black011602@gmail.com>
2022-04-09 20:12:24 +0800

    feat: add isAuthenticated to display different view templattes

--- models/user.js
+++ models/user.js
@@ -8,6 +8,14 @@ const userSchema = new Schema({
   name:{
     type: String,
     required: true
+  },
+  email:{
+    type: String,
+    required: true
+  },
+  password:{
+    type: String,
+    required: true
   }
 })
 
